6f2a182a2ae722998062d7de933105d00232ea6a
9-d-richardson <9.d.richardson@gmail.com>
2022-10-26 16:00:51 -0700

    Add custom 404 page

--- config/settings.py
+++ config/settings.py
@@ -93,7 +93,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 if DEBUG:
     DATABASES = {
         'default': {
-            'ENGINE': env.str('DATABASE_ENGINE'),
+            'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
