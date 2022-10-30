8e993f437c40faec3fd87e439d806a872ae2da8c
9-d-richardson <9.d.richardson@gmail.com>
2022-10-30 16:07:26 -0700

    Added HSTS related security settings

--- config/urls.py
+++ config/urls.py
@@ -19,7 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('control/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('management/', include('management.urls')),
     path('', include('timelines.urls')),
