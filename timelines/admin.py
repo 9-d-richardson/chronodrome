fb9e919e3e22b6f5f33d1ad97ae19e03c6eb2724
David Richardson <9.d.richardson@gmail.com>
2023-03-18 15:55:26 -0700

    further improvements to admin page

--- timelines/admin.py
+++ timelines/admin.py
@@ -1,17 +1,28 @@
 from django.contrib import admin
-from .models import Timeline, Entry
+from .models import Timeline, Entry, Divider, Image
 
 class EntryInline(admin.StackedInline):
 	model = Entry
+	extra = 0
+
+
+class DividerInline(admin.StackedInline):
+	model = Divider
+	extra = 0
+
+
+class ImageInline(admin.StackedInline):
+	model = Image
 	extra = 0
 	
 
 class TimelineAdmin(admin.ModelAdmin):
 	inlines = [
-		EntryInline,
+		EntryInline, DividerInline, ImageInline
 	]
-
-	list_display = ('title', 'creator', 'url', 'slug')
+	fields = ['title', 'creator', 'hidden', 'description', 'header_image', 
+		'header_caption', 'header_source', 'url', 'slug']
+	list_display = ('title', 'creator')
 
 admin.site.register(Timeline, TimelineAdmin)
 admin.site.register(Entry)
